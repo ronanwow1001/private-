00eb185743f3a85834ebec763bea2fbf209e8c5d
ronanwow1001 <ronancaleb123@gmail.com>
2019-07-11 16:07:32 -0700

    Add files via upload

---
+++ win32/TTRM_Engine.bat
@@ -0,0 +1,33 @@
+@echo off
+
+title Toontown Remodified Engine
+
+set /P ttrUsername="Username: " || ^
+set ttrmUsername=username
+set /P ttrmpassword="Password: " || ^
+set ttrmpassword=password
+set TTRM_PLAYCOOKIE=%ttrmUsername%
+set TTRM_PASSWORD=%ttrmPassword%
+set username=%ttrmUsername%
+set password=%ttrmpassword%
+set TTR_GAMESERVER=192.99.144.208
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+echo ===============================
+echo Starting Toontown Remodified - DEV...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttrmUsername%
+echo Client Agent IP: %TTRM_GAMESERVER%
+echo ===============================
+
+from toontown.toonbase import ToonBase.py
+
+echo starting the game...
+
+echo enjoy the game!
+
+goto server
+
+pause
